83af251aba78112a8480149dc92e0d03698e2048
yetugeng <yetugeng911@hotmail.com>
2017-07-13 22:51:39 +0800

    第一章 欢迎来到 Flask 的世界

--- micblog/app/__init__.py
+++ micblog/app/__init__.py
@@ -1,8 +1,4 @@
 # -*- coding:utf-8 -*-
-# 有__init__.py才能证明文件夹app1是个包，定义了包的属性和方法，当然也可以为空文件。
-# 如果没有本文件的，则app1只是个文件夹，不具备包的特性。
-# 当外部引用包app1时，就具备了如下初始化的环境：Flask，app2，views。
 from flask import Flask
-app = Flask(__name__)       #实例化了一个Flask，赋值给app2变量，该变量隶属app1包。
-app.config.from_object('config')
-from app import views    #引用了app1下面的views
+app = Flask(__name__)
+from app import views
--- micblog/app/views.py
+++ micblog/app/views.py
@@ -1,41 +1,6 @@
-# -*- coding: utf-8 -*-
-from flask import render_template, flash, redirect
-from forms import LoginForm
+#-*- coding:utf-8 -*-
 from app import app
-
-
+@app.route('/')
+@app.route('/index')
 def index():
-    user = { 'nickname': 'Miguel' } # 用户名
-    posts = [ # 提交内容
-        {
-            'author': { 'nickname': 'John' },
-            'body': 'Beautiful day in Portland!'
-        },
-        {
-            'author': { 'nickname': 'Susan' },
-            'body': 'The Avengers movie was so cool!'
-        }
-    ]
-    return render_template("index.html",
-                           title = 'Home',
-                           user = user,
-                           posts = posts)
-
-# @app.route('/login', methods = ['GET', 'POST'])
-# def login():
-#     form = LoginForm()
-#     return render_template('login.html',
-#         title = 'Sign In',
-#         form = form)
-
-@app.route('/login', methods = ['GET', 'POST'])
-def login():
-    form = LoginForm()
-    if form.validate_on_submit():
-        flash('Login requested for Name: ' + form.name.data)
-        flash('passwd: ' + str(form.password.data))
-        flash('remember_me: ' + str(form.remember_me.data))
-        return redirect('/index')
-    return render_template('login.html',
-                           title = 'Sign In',
-                           form = form)
+    return "Hello, World!"
